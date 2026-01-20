e7cda6fd843ebcc7a4a152eb83f93e015c0affcf
EgorZdayn <zamilevegor@gmail.com>
2026-01-20 15:30:13 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     symbols: ['STABLEUSDT', 'ETHUSDT', 'AUDIOUSDT'],
-    volumeMultiplier: 2.0,
+    volumeMultiplier: 5.0,
     checkInterval: 10000,
     historySize: 24,
-    telegramToken: "7965649045:AAFiiaHtGCIG35iCezTd3u_fdT09Da1UM4I",
-    telegramChatId: "-5030841108",
+    telegramToken: "8477737545:AAELGQ7lRMWFShNdB0KtnP_3k2CSiC6KQvU",
+    telegramChatId: "8477737545",
 };
